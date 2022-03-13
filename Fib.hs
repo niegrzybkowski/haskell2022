d0c2d59787d8045b19999a14adbc1dc9c7ab7e1e
niegrzybkowski <kacper.grzymkowski00@gmail.com>
2022-03-13 18:45:08 +0100

    Lab 02, Self-Task 1

--- Fib.hs
+++ Fib.hs
@@ -11,8 +11,20 @@ fibSensible :: Integer -> Integer
 fibSensible n 
     | n == 1 || n == 0 = 1
     | otherwise = fibLoop n 1 0
+    where
+        fibLoop :: Integer -> Integer -> Integer -> Integer 
+        fibLoop n_0 n_1 n_2 
+            | n_0 <= 1 = n_1 + n_2
+            | otherwise = fibLoop (n_0-1) (n_1 + n_2)  n_1
 
-fibLoop :: Integer -> Integer -> Integer -> Integer 
-fibLoop n n_1 n_2 
-    | n <= 1 = n_1 + n_2
-    | otherwise = fibLoop (n-1) (n_1 + n_2)  n_1
+fibSensible' :: Integer -> Integer
+fibSensible' n 
+    | n == 1 || n == 0 = 1
+    | otherwise = fibLoop' n 1 0
+        where
+            fibLoop' :: Integer -> Integer -> Integer -> Integer 
+            fibLoop' n_0 n_1 n_2 
+                | n_0 <= 1 = cum
+                | otherwise = cum `seq` fibLoop' (n_0-1) cum n_1
+                    where 
+                        cum = n_1 + n_2
